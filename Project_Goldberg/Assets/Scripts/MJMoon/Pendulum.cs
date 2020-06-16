a0b4939b92e8f1c67443d86ffd3c59bde9f09bae
mmj <mmj4594@naver.com>
2020-06-16 21:54:22 +0900

    Implement pendulum for function3_mmj

---
+++ Project_Goldberg/Assets/Scripts/MJMoon/Pendulum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    Transform tf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        //Debug.Log(tf.position);
+        //Debug.Log(tf.parent.transform.position);
+        tf.position = tf.parent.transform.position + new Vector3(3, -6, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
