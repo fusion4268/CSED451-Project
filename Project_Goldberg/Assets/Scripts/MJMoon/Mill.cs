7353372f23b412f4305ec726f6534fed34f3f65c
mmj <mmj4594@naver.com>
2020-06-24 16:50:38 +0900

    Assemble part 1, 2, 3

---
+++ Project_Goldberg/Assets/Scripts/MJMoon/Mill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mill : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "ball7")
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+        }
+    }
+}
